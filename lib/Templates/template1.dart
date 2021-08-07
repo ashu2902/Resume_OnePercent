69d352c305079d1cb111726c262fef19c3071a9a
unknown <sharmaashu.2902@gmail.com>
2021-08-08 03:34:36 +0530

    template1 ui in progess

--- lib/Templates/template1.dart
+++ lib/Templates/template1.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 class PdfApi {
-  static Future<File> generateText(String text) async {
+  static Future<File> generateText(
+      String name, address, phone, email, about) async {
     final pdf = pw.Document();
     pdf.addPage(
       pw.Page(
@@ -16,7 +19,71 @@ class PdfApi {
               //     style: pw.TextStyle(fontSize: 48),
               //   ),
               // ),
-              pw.Column(children: [pw.Text(text)])),
+              pw.Column(children: [
+                //Name
+                pw.Container(
+                    alignment: pw.Alignment.centerLeft,
+                    child: pw.Text(name, style: pw.TextStyle(fontSize: 20))),
+                pw.Divider(color: PdfColors.blue),
+                //Address, Phone , Email
+                pw.Container(
+                    child: pw.Row(children: [
+                  pw.Container(child: pw.Text(address)),
+                  pw.Container(
+                      height: 10, child: pw.VerticalDivider(width: 20)),
+                  pw.Container(child: pw.Text(phone)),
+                  pw.Container(
+                      height: 10, child: pw.VerticalDivider(width: 20)),
+                  pw.Container(child: pw.Text(email)),
+                ])),
+                pw.SizedBox(height: 30),
+                //About
+                pw.Column(
+                    mainAxisAlignment: pw.MainAxisAlignment.start,
+                    crossAxisAlignment: pw.CrossAxisAlignment.start,
+                    children: [
+                      pw.Container(
+                          alignment: pw.Alignment.topLeft,
+                          child: pw.Text('About',
+                              style: pw.TextStyle(fontSize: 18))),
+                      pw.Container(width: 30, child: pw.Divider())
+                    ]),
+
+                pw.Container(child: pw.Text(about)),
+                pw.SizedBox(height: 30),
+
+                //Experience and Education
+                pw.Row(
+                    mainAxisAlignment: pw.MainAxisAlignment.spaceEvenly,
+                    children: [
+                      //Experience,Skills
+                      pw.Expanded(
+                          child: pw.Container(
+                              alignment: pw.Alignment.centerLeft,
+                              child: pw.Column(
+                                  mainAxisAlignment: pw.MainAxisAlignment.start,
+                                  crossAxisAlignment:
+                                      pw.CrossAxisAlignment.start,
+                                  children: [
+                                    pw.Text('Experience',
+                                        style: pw.TextStyle(fontSize: 18)),
+                                    pw.Container(width: 30, child: pw.Divider())
+                                  ]))),
+                      //Education, Projects,
+                      pw.Expanded(
+                          child: pw.Container(
+                              alignment: pw.Alignment.centerLeft,
+                              child: pw.Column(
+                                  mainAxisAlignment: pw.MainAxisAlignment.start,
+                                  crossAxisAlignment:
+                                      pw.CrossAxisAlignment.start,
+                                  children: [
+                                    pw.Text('Education',
+                                        style: pw.TextStyle(fontSize: 18)),
+                                    pw.Container(width: 30, child: pw.Divider())
+                                  ])))
+                    ])
+              ])),
     );
 
     return saveDocument(name: 'my_resume.pdf', pdf: pdf);
