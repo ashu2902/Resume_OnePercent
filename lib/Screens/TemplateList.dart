c0cdb2c1b869e294d01ffec2a8643548836019ad
Raj Pratap Soni <80382756+Rajpratapsoni@users.noreply.github.com>
2021-08-17 16:25:40 +0530

    tamplate3

--- lib/Screens/TemplateList.dart
+++ lib/Screens/TemplateList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resume/Templates/Tamplate3.dart';
 import 'package:resume/Templates/template1.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,11 +19,12 @@ class _TemplateListState extends State<TemplateList> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+
               Container(
                 child: TextButton(
                   onPressed: () async {
                     SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
+                    await SharedPreferences.getInstance();
                     final pdfFile = await PdfApi.generateText(
                         '${prefs.getString('name')}',
                         '${prefs.getString('address')}',
@@ -40,6 +42,30 @@ class _TemplateListState extends State<TemplateList> {
                     PdfApi.openFile(pdfFile);
                   },
                   child: Text('Template1'),
+                ),
+              ),
+              Container(
+                child: TextButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                    await SharedPreferences.getInstance();
+                    final pdf = await Pdf.generateText(
+                        '${prefs.getString('name')}',
+                        '${prefs.getString('address')}',
+                        '${prefs.getInt('phone')}',
+                        '${prefs.getString('email')}',
+                        "${prefs.getString('about')}",
+                        'Sample Title-1',
+                        'Sample Organization',
+                        '01-02-20XX',
+                        'skill-1,skill-2,skill-3',
+                        '${prefs.getString('Instname')}',
+                        '${prefs.getString('course')}',
+                        '${prefs.getString('Eddate1')}',
+                        '${prefs.getString('Eddate2')}');
+                    PdfApi.openFile(pdf);
+                  },
+                  child: Text('Template3'),
                 ),
               ),
             ],
