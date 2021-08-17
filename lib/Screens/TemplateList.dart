7a059990fc7dd3d17f918656816143d2dd9c279b
unknown <sharmaashu.2902@gmail.com>
2021-08-17 16:59:12 +0530

    raw

--- lib/Screens/TemplateList.dart
+++ lib/Screens/TemplateList.dart
@@ -18,50 +18,68 @@ class _TemplateListState extends State<TemplateList> {
     return Scaffold(
       body: Container(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //template 1
-              Container(
-                child: TextButton(
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    final pdfFile = await Template1.generateText(
-                        '${prefs.getString('name')}',
-                        '${prefs.getString('address')}',
-                        '${prefs.getInt('phone')}',
-                        '${prefs.getString('email')}',
-                        "${prefs.getString('about')}",
-                        'Sample Title-1',
-                        'Sample Organization',
-                        '01-02-20XX',
-                        'skill-1,skill-2,skill-3',
-                        '${prefs.getString('Instname')}',
-                        '${prefs.getString('course')}',
-                        '${prefs.getString('Eddate1')}',
-                        '${prefs.getString('Instname2')}',
-                        '${prefs.getString('course2')}',
-                        '${prefs.getString('Eddate2')}');
-                    Template1.openFile(pdfFile);
-                  },
-                  child: Text('Template1'),
-                ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //template 1
+            Container(
+              child: TextButton(
+                onPressed: () async {
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  final pdfFile = await Template1.generateText(
+                      '${prefs.getString('name')}',
+                      '${prefs.getString('address')}',
+                      '${prefs.getInt('phone')}',
+                      '${prefs.getString('email')}',
+                      "${prefs.getString('about')}",
+                      'Sample Title-1',
+                      'Sample Organization',
+                      '01-02-20XX',
+                      'skill-1,skill-2,skill-3',
+                      '${prefs.getString('Instname')}',
+                      '${prefs.getString('course')}',
+                      '${prefs.getString('Eddate1')}',
+                      '${prefs.getString('Instname2')}',
+                      '${prefs.getString('course2')}',
+                      '${prefs.getString('Eddate2')}');
+                  Template1.openFile(pdfFile);
+                },
+                child: Text('Template1'),
               ),
-              Container(
-                child: TextButton(
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    final pdfFile2 = await PdfApi2();
-                    PdfApi2.openFile(pdfFile2);
-                  },
-                  child: Text('template2'),
-                ),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: () async {
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  final pdfFile2 = await PdfApi2.generateText2(
+                      'Sample Name',
+                      "Sample Address",
+                      "Sample Phone",
+                      "Sample Email",
+                      "Sample Website",
+                      "Company Name 1",
+                      "Location 1 ",
+                      "Start Date1",
+                      'enddate1',
+                      'companyname2',
+                      'location2',
+                      'startdate2',
+                      'enddate2',
+                      'schoolname',
+                      'slocation',
+                      'sdegree',
+                      'referenceName',
+                      'rCompany',
+                      'rContactInformation');
+                  PdfApi2.openFile(pdfFile2);
+                },
+                child: Text('template2'),
               ),
-            ],
-          )
-        ),
+            ),
+          ],
+        )),
       ),
     );
   }
