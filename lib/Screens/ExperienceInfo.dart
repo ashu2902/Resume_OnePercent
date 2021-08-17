881b2d8ee154b9aaa6a40748f8881f0f795170a2
unknown <sharmaashu.2902@gmail.com>
2021-08-17 16:10:38 +0530

    creating templates

--- lib/Screens/ExperienceInfo.dart
+++ lib/Screens/ExperienceInfo.dart
@@ -6,8 +6,55 @@ class ExperienceInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text('Experience Info'),
+      appBar: AppBar(
+        title: Text('Experience'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFields(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFields extends StatefulWidget {
+  const TextFields({Key? key}) : super(key: key);
+
+  @override
+  _TextFieldsState createState() => _TextFieldsState();
+}
+
+class _TextFieldsState extends State<TextFields> {
+  @override
+  Widget build(BuildContext context) {
+    final _width = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: _width,
+      child: Center(
+        child: Container(
+          width: _width / 1.8,
+          child: Column(
+            children: [
+              Text('Experience:1'),
+              TextField(),
+              TextField(),
+              TextField(),
+            ],
+          ),
+        ),
       ),
     );
   }
