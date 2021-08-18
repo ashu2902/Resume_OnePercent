44ca1ecb64b0651ef07c48893b967bd49e37f3d1
unknown <sharmaashu.2902@gmail.com>
2021-08-18 22:17:03 +0530

    raw

--- lib/Screens/TemplateList.dart
+++ lib/Screens/TemplateList.dart
@@ -18,121 +18,109 @@ class _TemplateListState extends State<TemplateList> {
     return Scaffold(
       body: Container(
         child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //template 1
-            Container(
-              child: TextButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  final pdfFile = await Template1.generateText(
-                      '${prefs.getString('name')}',
-                      '${prefs.getString('address')}',
-                      '${prefs.getInt('phone')}',
-                      '${prefs.getString('email')}',
-                      "${prefs.getString('about')}",
-                      "${prefs.getString('JobTitle1')}",
-                      "${prefs.getString('CompName1')}",
-                      "${prefs.getString('CompStartDate1')}",
-                      "${prefs.getString('CompEndDate1')}",
-                      "${prefs.getString('JobTitle2')}",
-                      "${prefs.getString('CompName2')}",
-                      "${prefs.getString('CompStartDate2')}",
-                      "${prefs.getString('CompEndDate2')}",
-                      'skill-1,skill-2,skill-3',
-                      '${prefs.getString('Instname')}',
-                      '${prefs.getString('course')}',
-                      '${prefs.getString('Eddate1')}',
-                      'About Information ',
-                      '${prefs.getString('Instname2')}',
-                      '${prefs.getString('course2')}',
-                      '${prefs.getString('Eddate2')}',
-                      'ProjectTitle1',
-                      'ProjectDate1',
-                      'ProjectTitle2',
-                      'ProjectDate2');
-                  Template1.openFile(pdfFile);
-                },
-                child: Text('Template1'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //template 1
+              Container(
+                child: TextButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    final pdfFile = await Template1.generateText(
+                        '${prefs.getString('name')}',
+                        '${prefs.getString('address')}',
+                        '${prefs.getInt('phone')}',
+                        '${prefs.getString('email')}',
+                        "${prefs.getString('about')}",
+                        "${prefs.getString('JobTitle1')}",
+                        "${prefs.getString('CompName1')}",
+                        "${prefs.getString('CompStartDate1')}",
+                        "${prefs.getString('CompEndDate1')}",
+                        "${prefs.getString('JobTitle2')}",
+                        "${prefs.getString('CompName2')}",
+                        "${prefs.getString('CompStartDate2')}",
+                        "${prefs.getString('CompEndDate2')}",
+                        'skill-1,skill-2,skill-3',
+                        '${prefs.getString('Instname')}',
+                        '${prefs.getString('course')}',
+                        '${prefs.getString('Eddate1')}',
+                        'About Information ',
+                        '${prefs.getString('Instname2')}',
+                        '${prefs.getString('course2')}',
+                        '${prefs.getString('Eddate2')}',
+                        'ProjectTitle1',
+                        'ProjectDate1',
+                        'ProjectTitle2',
+                        'ProjectDate2');
+                    Template1.openFile(pdfFile);
+                  },
+                  child: Text('Template1'),
+                ),
               ),
-            ),
-            //template4
-            Container(
-              child: TextButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  final template4 = await Template4.generateTemplate(
-                      '${prefs.getString('name')}',
-                      '${prefs.getString('address')}',
-                      '${prefs.getInt('phone')}',
-                      '${prefs.getString('email')}',
-                      "${prefs.getString('about')}",
-                      'Sample Title-1',
-                      'Sample Organization',
-                      '01-02-20XX',
-                      'skill-1,skill-2,skill-3',
-                      '${prefs.getString('Instname')}',
-                      '${prefs.getString('course')}',
-                      '${prefs.getString('Eddate1')}',
-                      '${prefs.getString('Eddate2')}');
-                  Template4.openFile(template4);
-                },
-                child: Text('Template4'),
+              //template4
+              Container(
+                child: TextButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    final template4 = await Template4.generateTemplate(
+                        '${prefs.getString('name')}',
+                        '${prefs.getString('address')}',
+                        '${prefs.getInt('phone')}',
+                        '${prefs.getString('email')}',
+                        "${prefs.getString('about')}",
+                        'Sample Title-1',
+                        'Sample Organization',
+                        '01-02-20XX',
+                        'skill-1,skill-2,skill-3',
+                        '${prefs.getString('Instname')}',
+                        '${prefs.getString('course')}',
+                        '${prefs.getString('Eddate1')}',
+                        '${prefs.getString('Eddate2')}');
+                    Template4.openFile(template4);
+                  },
+                  child: Text('Template4'),
+                ),
               ),
-            ),
-            Container(
-              child: TextButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  final pdf = await Pdf.generateText(
-                      '${prefs.getString('name')}',
-                      '${prefs.getString('address')}',
-                      '${prefs.getInt('phone')}',
-                      '${prefs.getString('email')}',
-                      "${prefs.getString('about')}",
-                      'Sample Title-1',
-                      'Sample Organization',
-                      '01-02-20XX',
-                      'skill-1,skill-2,skill-3',
-                      '${prefs.getString('Instname')}',
-                      '${prefs.getString('course')}',
-                      '${prefs.getString('Eddate1')}',
-                      '${prefs.getString('Eddate2')}');
-                  Pdf.openFile(pdf);
-                },
-                child: Text('Template3'),
+              Container(
+                child: TextButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    final pdf = await Pdf.generateText(
+                        '${prefs.getString('name')}',
+                        '${prefs.getString('address')}',
+                        '${prefs.getInt('phone')}',
+                        '${prefs.getString('email')}',
+                        "${prefs.getString('about')}",
+                        'Sample Title-1',
+                        'Sample Organization',
+                        '01-02-20XX',
+                        'skill-1,skill-2,skill-3',
+                        '${prefs.getString('Instname')}',
+                        '${prefs.getString('course')}',
+                        '${prefs.getString('Eddate1')}',
+                        '${prefs.getString('Eddate2')}');
+                    Pdf.openFile(pdf);
+                  },
+                  child: Text('Template3'),
+                ),
               ),
-            ),
-            Container(
-              child: TextButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                  await SharedPreferences.getInstance();
-                  final template6 = await Template6.generateTemplate(
-                      '${prefs.getString('name')}',
-                      '${prefs.getString('address')}',
-                      '${prefs.getInt('phone')}',
-                      '${prefs.getString('email')}',
-                      "${prefs.getString('about')}",
-                      'Sample Title-1',
-                      'Sample Organization',
-                      '01-02-20XX',
-                      'skill-1,skill-2,skill-3',
-                      '${prefs.getString('Instname')}',
-                      '${prefs.getString('course')}',
-                      '${prefs.getString('Eddate1')}',
-                      '${prefs.getString('Eddate2')}');
-                  Template6.openFile(template6);
-                },
-                child: Text('Template6'),
+              Container(
+                child: TextButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    final template6 = await Template6.generateTemplate();
+                    Template6.openFile(template6);
+                  },
+                  child: Text('Template6'),
+                ),
               ),
-            ),
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
