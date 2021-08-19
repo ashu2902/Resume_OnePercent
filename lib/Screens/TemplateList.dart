3705e167ba309f47fb40c9400e8c2579d399128d
unknown <sharmaashu.2902@gmail.com>
2021-08-19 20:52:06 +0530

    template list reorganised

--- lib/Screens/TemplateList.dart
+++ lib/Screens/TemplateList.dart
@@ -138,109 +138,7 @@ class _TemplateListState extends State<TemplateList> {
                     Pdf.openFile(pdf);
                   },
                   title: 'Template3'),
-              //Template4
-              TemplateButton(
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    final pdfFile = await Template1.generateText(
-                        '${prefs.getString('name')}',
-                        '${prefs.getString('address')}',
-                        '${prefs.getInt('phone')}',
-                        '${prefs.getString('email')}',
-                        "${prefs.getString('about')}",
-                        "${prefs.getString('JobTitle1')}",
-                        "${prefs.getString('CompName1')}",
-                        "${prefs.getString('CompStartDate1')}",
-                        "${prefs.getString('CompEndDate1')}",
-                        "${prefs.getString('JobTitle2')}",
-                        "${prefs.getString('CompName2')}",
-                        "${prefs.getString('CompStartDate2')}",
-                        "${prefs.getString('CompEndDate2')}",
-                        '${prefs.getString('Skill1')},${prefs.getString('Skill2')},${prefs.getString('Skill3')}',
-                        '${prefs.getString('Instname')}',
-                        '${prefs.getString('course')}',
-                        '${prefs.getString('Eddate1')}',
-                        '${prefs.getString('Summary1')}',
-                        '${prefs.getString('Instname2')}',
-                        '${prefs.getString('course2')}',
-                        '${prefs.getString('Eddate2')}',
-                        '${prefs.getString('Summary2')}',
-                        '${prefs.getString('ProjectTitle1')}',
-                        '${prefs.getString('ProjectDesc1')}',
-                        '${prefs.getString('ProjectTitle2')}',
-                        '${prefs.getString('ProjectDesc2')}');
-                    Template1.openFile(pdfFile);
-                  },
-                  title: 'Template1'),
-              //template2
-              TemplateButton(
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    final template2 = await PdfApi2.generateText2(
-                      '${prefs.getString('name')}',
-                      "${prefs.getString('address')}",
-                      "${prefs.getInt('phone')}",
-                      "${prefs.getString('email')}",
-                      "${prefs.getString('about')}",
-                      "${prefs.getString('CompName1')}",
-                      "${prefs.getString('CompLocation1')}",
-                      "${prefs.getString('CompStartDate1')}",
-                      "${prefs.getString('CompEndDate1')}",
-                      '${prefs.getString('Summary1')}',
-                      "${prefs.getString('CompName2')}",
-                      "${prefs.getString('CompLocation1')}",
-                      "${prefs.getString('CompStartDate1')}",
-                      "${prefs.getString('CompEndDate2')}",
-                      '${prefs.getString('Summary2')}',
-                      "${prefs.getString('Instname')}",
-                      "${prefs.getString('EdLocation1')}",
-                      "${prefs.getString('course')}",
-                      "${prefs.getString('EdSummary1')}",
-                      "${prefs.getString('Instname2')}",
-                      "${prefs.getString('EdLocation2')}",
-                      "${prefs.getString('course2')}",
-                      "${prefs.getString('EdSummary2')}",
-                      '${prefs.getString('Skill1')},${prefs.getString('Skill2')},${prefs.getString('Skill3')}',
-                    );
-                    PdfApi2.openFile(template2);
-                  },
-                  title: 'Template2'),
-              //Template 3
-              TemplateButton(
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    final pdf = await Pdf.generateText(
-                      '${prefs.getString('name')}',
-                      '${prefs.getString('address')}',
-                      "${prefs.getInt('phone')}",
-                      "${prefs.getString('email')}",
-                      "${prefs.getString('about')}",
-                      "${prefs.getString('CompStartDate1')}",
-                      "${prefs.getString('JobTitle1')}",
-                      "${prefs.getString('CompName1')}",
-                      '${prefs.getString('Summary1')}',
-                      "${prefs.getString('CompStartDate2')}",
-                      "${prefs.getString('JobTitle2')}",
-                      "${prefs.getString('CompName2')}",
-                      '${prefs.getString('Summary2')}',
-                      '${prefs.getString('ProjectTitle1')}',
-                      '${prefs.getString('ProjectDesc1')}',
-                      '${prefs.getString('ProjectTitle2')}',
-                      '${prefs.getString('ProjectDesc2')}',
-                      '${prefs.getString('Skill1')},${prefs.getString('Skill2')},${prefs.getString('Skill3')}',
-                      '${prefs.getString('Instname')}',
-                      "${prefs.getString('course')}",
-                      '${prefs.getString('Eddate1')}',
-                      '${prefs.getString('Instname2')}',
-                      "${prefs.getString('course2')}",
-                      '${prefs.getString('Eddate2')}',
-                    );
-                    Pdf.openFile(pdf);
-                  },
-                  title: 'Template3'),
+
               //Template4
               TemplateButton(
                   onPressed: () async {
@@ -339,15 +237,8 @@ class _TemplateListState extends State<TemplateList> {
                 },
                 title: 'Template7',
               ),
+              //Template7
 
-              TemplateButton(
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    final pdf = await Template14.generateText();
-                    Template14.openFile(pdf);
-                  },
-                  title: 'Template 14'),
               TemplateButton(
                   onPressed: () async {
                     SharedPreferences prefs =
@@ -383,12 +274,8 @@ class _TemplateListState extends State<TemplateList> {
                     );
                     Template11.openFile(pdf11);
                   },
-                  title: 'Template5'),
-              //Template6
-              TemplateButton(
-                onPressed: () async {},
-                title: 'Template11',
-              ),
+                  title: 'Template 11'),
+
               //Template12
               TemplateButton(
                 onPressed: () async {
@@ -438,112 +325,6 @@ class _TemplateListState extends State<TemplateList> {
                 title: 'Template12',
               ),
 
-              //Template7
-              TemplateButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  final pdf = await Template7.generateText();
-                  Template7.openFile(pdf);
-                },
-                title: 'template7',
-              ),
-              TemplateButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  final pdf = await Template14.generateText();
-                  Template14.openFile(pdf);
-                },
-                title: 'template14',
-              ),
-              //Template11
-              TemplateButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  final pdf11 = await Template11.generateTemplate(
-                    'John Smith',
-                    '123 Address',
-                    '+91 123456789',
-                    'email@email.com',
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Ut enim ad minim veniam, quis  nostrud exercitation ullamco',
-                    'Redford & Sons, Boston, MA',
-                    'Administrator Assistant',
-                    'Sept 2015 - present',
-                    [
-                      'Notable Achievement/Responsibility',
-                      'Notable Achievement/Responsibility',
-                      'Notable Achievement/Responsibility'
-                    ],
-                    'Blueford & Fathers, Coston, DA',
-                    'Administrator of the coffee machine',
-                    'Dec 1996 - 2014',
-                    [
-                      'Notable Achievement/Responsibility',
-                      'Notable Achievement/Responsibility',
-                      'Notable Achievement/Responsibility'
-                    ],
-                    'River Brooks University',
-                    'Bachelor of Arts',
-                    'Honors cum laude (GPA 3.5/4.0)',
-                    'Expert in Microsoft Office',
-                    'Bilingual in English and Spanish',
-                    'HTML, CSS and Javascript',
-                  );
-                  Template11.openFile(pdf11);
-                },
-                title: 'template11',
-              ),
-              //Template12
-              TemplateButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  final pdf12 = await Template12.generateTemplate(
-                    'John Smith',
-                    'IT Project Manager',
-                    'Portland, MA',
-                    '123-456-789',
-                    'email@email.com',
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
-                    '2006-12',
-                    'Some Company',
-                    'Senior Project Manager',
-                    [
-                      'Notable Achievement/Responsibility',
-                      'Notable Achievement/Responsibility',
-                      'Notable Achievement/Responsibility'
-                    ],
-                    '2003-11',
-                    'Some Company',
-                    'Junior Project Manager',
-                    [
-                      'Notable Achievement/Responsibility',
-                      'Notable Achievement/Responsibility',
-                      'Notable Achievement/Responsibility'
-                    ],
-                    '1999-02',
-                    'Master of Computer Science',
-                    'University of Maryland',
-                    [
-                      'Graduated with a pineapple',
-                      'Member of the Homeless Society'
-                    ],
-                    '1996-07',
-                    'Bachelor of Computer Science',
-                    'University of Maryland',
-                    [
-                      'Member of the Student Association of Blind People',
-                      'Managed student project of 50+ rodents',
-                    ],
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-                  );
-                  Template12.openFile(pdf12);
-                },
-                title: 'template12',
-              ),
-
               //Template13
               TemplateButton(
                 onPressed: () async {
@@ -582,6 +363,16 @@ class _TemplateListState extends State<TemplateList> {
                   Template12.openFile(pdf13);
                 },
                 title: 'Template13',
+              ),
+              //Template 14
+              TemplateButton(
+                onPressed: () async {
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  final pdf = await Template14.generateText();
+                  Template14.openFile(pdf);
+                },
+                title: 'template14',
               ),
               //Template15
               TemplateButton(
