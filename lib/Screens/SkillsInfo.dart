5b5cbf7be59bd995d710d562a9cdb23be09b3e1e
unknown <sharmaashu.2902@gmail.com>
2021-08-19 16:09:46 +0530

    TemplateList UI changed

--- lib/Screens/SkillsInfo.dart
+++ lib/Screens/SkillsInfo.dart
@@ -11,6 +11,7 @@ class SkillsInfo extends StatefulWidget {
 Future<bool> saveData(skill1, skill2, skill3) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   prefs.setString("Skill1", skill1);
+
   prefs.setString("Skill2", skill2);
   prefs.setString("Skill3", skill3);
   // ignore: deprecated_member_use
--- lib/Screens/TemplateList.dart
+++ lib/Screens/TemplateList.dart
@@ -26,8 +26,7 @@ class _TemplateListState extends State<TemplateList> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             //template 1
-            Container(
-              child: TextButton(
+            TemplateButton(
                 onPressed: () async {
                   SharedPreferences prefs =
                       await SharedPreferences.getInstance();
@@ -60,49 +59,45 @@ class _TemplateListState extends State<TemplateList> {
                       '${prefs.getString('ProjectDesc2')}');
                   Template1.openFile(pdfFile);
                 },
-                child: Text('Template1'),
-              ),
-            ),
+                child: Text('Template1')),
 
             //template2
-            Container(
-              child: TextButton(
-                  onPressed: () async {
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    final template2 = await PdfApi2.generateText2(
-                      '${prefs.getString('name')}',
-                      "${prefs.getString('address')}",
-                      "${prefs.getInt('phone')}",
-                      "${prefs.getString('email')}",
-                      "${prefs.getString('about')}",
-                      "${prefs.getString('CompName1')}",
-                      "${prefs.getString('CompLocation1')}",
-                      "${prefs.getString('CompStartDate1')}",
-                      "${prefs.getString('CompEndDate1')}",
-                      '${prefs.getString('Summary1')}',
-                      "${prefs.getString('CompName2')}",
-                      "${prefs.getString('CompLocation1')}",
-                      "${prefs.getString('CompStartDate1')}",
-                      "${prefs.getString('CompEndDate2')}",
-                      '${prefs.getString('Summary2')}',
-                      "${prefs.getString('Instname')}",
-                      "${prefs.getString('EdLocation1')}",
-                      "${prefs.getString('course')}",
-                      "${prefs.getString('EdSummary1')}",
-                      "${prefs.getString('Instname2')}",
-                      "${prefs.getString('EdLocation2')}",
-                      "${prefs.getString('course2')}",
-                      "${prefs.getString('EdSummary2')}",
-                      '${prefs.getString('Skill1')},${prefs.getString('Skill2')},${prefs.getString('Skill3')}',
-                    );
-                    PdfApi2.openFile(template2);
-                  },
-                  child: Text('Template2')),
-            ),
+            TemplateButton(
+                onPressed: () async {
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  final template2 = await PdfApi2.generateText2(
+                    '${prefs.getString('name')}',
+                    "${prefs.getString('address')}",
+                    "${prefs.getInt('phone')}",
+                    "${prefs.getString('email')}",
+                    "${prefs.getString('about')}",
+                    "${prefs.getString('CompName1')}",
+                    "${prefs.getString('CompLocation1')}",
+                    "${prefs.getString('CompStartDate1')}",
+                    "${prefs.getString('CompEndDate1')}",
+                    '${prefs.getString('Summary1')}',
+                    "${prefs.getString('CompName2')}",
+                    "${prefs.getString('CompLocation1')}",
+                    "${prefs.getString('CompStartDate1')}",
+                    "${prefs.getString('CompEndDate2')}",
+                    '${prefs.getString('Summary2')}',
+                    "${prefs.getString('Instname')}",
+                    "${prefs.getString('EdLocation1')}",
+                    "${prefs.getString('course')}",
+                    "${prefs.getString('EdSummary1')}",
+                    "${prefs.getString('Instname2')}",
+                    "${prefs.getString('EdLocation2')}",
+                    "${prefs.getString('course2')}",
+                    "${prefs.getString('EdSummary2')}",
+                    '${prefs.getString('Skill1')},${prefs.getString('Skill2')},${prefs.getString('Skill3')}',
+                  );
+                  PdfApi2.openFile(template2);
+                },
+                child: Text('Template2')),
+
             //Template 3
-            Container(
-              child: TextButton(
+            TemplateButton(
                 onPressed: () async {
                   SharedPreferences prefs =
                       await SharedPreferences.getInstance();
@@ -134,12 +129,9 @@ class _TemplateListState extends State<TemplateList> {
                   );
                   Pdf.openFile(pdf);
                 },
-                child: Text('Template3'),
-              ),
-            ),
+                child: Text('Template3')),
             //Template4
-            Container(
-              child: TextButton(
+            TemplateButton(
                 onPressed: () async {
                   SharedPreferences prefs =
                       await SharedPreferences.getInstance();
@@ -166,12 +158,9 @@ class _TemplateListState extends State<TemplateList> {
                       '${prefs.getString('Eddate2')}');
                   Template4.openFile(template4);
                 },
-                child: Text('Template4'),
-              ),
-            ),
+                child: Text('Template4')),
             //Template5
-            Container(
-              child: TextButton(
+            TemplateButton(
                 onPressed: () async {
                   SharedPreferences prefs =
                       await SharedPreferences.getInstance();
@@ -194,47 +183,51 @@ class _TemplateListState extends State<TemplateList> {
                   );
                   Template5.openFile(template5);
                 },
-                child: Text('Template5'),
-              ),
-            ),
+                child: Text('Template5')),
+
             //Template6
-            Container(
-              child: TextButton(
+            TemplateButton(
                 onPressed: () async {
                   SharedPreferences prefs =
                       await SharedPreferences.getInstance();
                   final template6 = await Template6.generateTemplate();
                   Template6.openFile(template6);
                 },
-                child: Text('Template6'),
-              ),
-            ),
+                child: Text('Template6')),
+
             //Template7
-            Container(
-              child: TextButton(
-                onPressed: () async {
-                  SharedPreferences prefs =
-                      await SharedPreferences.getInstance();
-                  final pdf = await Template7.generateText();
-                  Template7.openFile(pdf);
-                },
-                child: Text('Template7'),
-              ),
+            TemplateButton(
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                final pdf = await Template7.generateText();
+                Template7.openFile(pdf);
+              },
+              child: Text('Template7'),
             ),
-            Container(
-              child: TextButton(
+
+            TemplateButton(
                 onPressed: () async {
                   SharedPreferences prefs =
                       await SharedPreferences.getInstance();
                   final pdf = await Template14.generateText();
                   Template14.openFile(pdf);
                 },
-                child: Text('Template14'),
-              ),
-            ),
+                child: Text('Template 14'))
           ],
         )),
       ),
+    );
+  }
+}
+
+class TemplateButton extends StatelessWidget {
+  TemplateButton({required this.onPressed, required this.child});
+  final onPressed;
+  final child;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ElevatedButton(onPressed: this.onPressed, child: this.child),
     );
   }
 }
