1e9347268c6e1200e6659128896045fc38b60163
unknown <sharmaashu.2902@gmail.com>
2021-08-27 18:12:40 +0530

    save functionality done

--- lib/Templates/template1.dart
+++ lib/Templates/template1.dart
@@ -241,135 +241,6 @@ class Temp1 extends StatefulWidget {
   final projectDesc2;
 
   final pdf = pw.Document();
-  generateText() {
-    pdf.addPage(
-      pw.Page(
-        build: (context) => pw.Column(children: [
-          //Name
-          pw.Container(
-              alignment: pw.Alignment.centerLeft,
-              child: pw.Text(name, style: pw.TextStyle(fontSize: 20))),
-          pw.Divider(color: PdfColors.blue),
-          //Address, Phone , Email
-          pw.Container(
-              child: pw.Row(children: [
-            pw.Container(child: pw.Text(address)),
-            pw.Container(height: 10, child: pw.VerticalDivider(width: 20)),
-            pw.Container(child: pw.Text(phone)),
-            pw.Container(height: 10, child: pw.VerticalDivider(width: 20)),
-            pw.Container(child: pw.Text(email)),
-          ])),
-          pw.SizedBox(height: 30),
-          //About
-          pw.Column(
-              mainAxisAlignment: pw.MainAxisAlignment.start,
-              crossAxisAlignment: pw.CrossAxisAlignment.start,
-              children: [
-                pw.Container(
-                    alignment: pw.Alignment.topLeft,
-                    child: pw.Text('About', style: pw.TextStyle(fontSize: 18))),
-                pw.Container(width: 30, child: pw.Divider())
-              ]),
-
-          pw.Container(child: pw.Text(about)),
-          pw.SizedBox(height: 30),
-
-          //Experience and Education
-          pw.Row(
-              mainAxisAlignment: pw.MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: pw.CrossAxisAlignment.start,
-              children: [
-                //Experience,Skills
-                pw.Expanded(
-                    child: pw.Container(
-                        alignment: pw.Alignment.centerLeft,
-                        child: pw.Column(
-                            mainAxisAlignment: pw.MainAxisAlignment.start,
-                            crossAxisAlignment: pw.CrossAxisAlignment.start,
-                            children: [
-                              pw.Text('Experience',
-                                  style: pw.TextStyle(fontSize: 18)),
-                              pw.Container(width: 30, child: pw.Divider()),
-                              pw.Container(
-                                  child: pw.Text('$jobTitle1, $organization1',
-                                      style: pw.TextStyle(
-                                          fontSize: 16,
-                                          color: PdfColors.blue300))),
-                              //Date
-                              pw.Text('Date- $startdate1 to $enddate1'),
-                              //Achievement or Responsibility
-                              pw.Container(
-                                  height: 51,
-                                  child: pw.Bullet(text: experienceAbout1)),
-                              pw.SizedBox(height: 30),
-
-                              pw.Text('$jobTitle2, $organization2',
-                                  style: pw.TextStyle(
-                                      fontSize: 16, color: PdfColors.blue300)),
-                              //Date
-                              pw.Text('Date- $startdate2 to $enddate2'),
-                              //Achievement or Responsibility
-                              pw.Container(
-                                  height: 51,
-                                  child: pw.Bullet(text: experienceAbout2)),
-                              pw.SizedBox(height: 30),
-
-                              //SKILLS
-                              pw.Text('Skills',
-                                  style: pw.TextStyle(fontSize: 18)),
-                              pw.Container(width: 30, child: pw.Divider()),
-                              pw.Text(skills),
-                            ]))),
-                //Education, Projects,
-                pw.Expanded(
-                    child: pw.Container(
-                        alignment: pw.Alignment.centerLeft,
-                        child: pw.Column(
-                            mainAxisAlignment: pw.MainAxisAlignment.start,
-                            crossAxisAlignment: pw.CrossAxisAlignment.start,
-                            children: [
-                              pw.Text('Education',
-                                  style: pw.TextStyle(fontSize: 18)),
-                              pw.Container(width: 30, child: pw.Divider()),
-                              pw.Text(instName1,
-                                  style: pw.TextStyle(
-                                      fontSize: 16, color: PdfColors.blue300)),
-                              pw.Text(course1), pw.SizedBox(height: 10),
-
-                              pw.Text('Date- $edDate1'),
-                              pw.SizedBox(height: 15),
-                              pw.Text(instName2,
-                                  style: pw.TextStyle(
-                                      fontSize: 16, color: PdfColors.blue300)),
-                              pw.Text(course2),
-                              pw.Text('Date- $edDate2'),
-                              pw.SizedBox(height: 30),
-
-                              //Projects&Volunteer
-                              pw.Text('Projects & Volunteer',
-                                  style: pw.TextStyle(fontSize: 18)),
-                              pw.Container(width: 30, child: pw.Divider()),
-                              pw.SizedBox(height: 20),
-
-                              pw.Text('$projTitle1',
-                                  style: pw.TextStyle(
-                                      fontSize: 16, color: PdfColors.blue300)),
-
-                              //Achievement or Responsibility
-                              pw.Bullet(text: '$projectDesc1'),
-                              pw.SizedBox(height: 20),
-                              pw.Text('$projTitle2',
-                                  style: pw.TextStyle(
-                                      fontSize: 16, color: PdfColors.blue300)),
-
-                              //Achievement or Responsibility
-                              pw.Bullet(text: '$projectDesc2'),
-                            ])))
-              ]),
-        ]),
-      ),
-    );
-  }
 
   @override
   _Temp1State createState() => _Temp1State();
@@ -380,27 +251,188 @@ class _Temp1State extends State<Temp1> {
   @override
   void initState() {
     super.initState();
-    widget.generateText();
+    generateText();
     savePdf();
   }
 
+  generateText() {
+    widget.pdf.addPage(
+        pw.Page(
+          build: (context) => pw.Column(children: [
+            //Name
+            pw.Container(
+                alignment: pw.Alignment.centerLeft,
+                child: pw.Text(widget.name, style: pw.TextStyle(fontSize: 20))),
+            pw.Divider(color: PdfColors.blue),
+            //Address, Phone , Email
+            pw.Container(
+                child: pw.Row(children: [
+              pw.Container(child: pw.Text(widget.address)),
+              pw.Container(height: 10, child: pw.VerticalDivider(width: 20)),
+              pw.Container(child: pw.Text(widget.phone)),
+              pw.Container(height: 10, child: pw.VerticalDivider(width: 20)),
+              pw.Container(child: pw.Text(widget.email)),
+            ])),
+            pw.SizedBox(height: 30),
+            //About
+            pw.Column(
+                mainAxisAlignment: pw.MainAxisAlignment.start,
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: [
+                  pw.Container(
+                      alignment: pw.Alignment.topLeft,
+                      child:
+                          pw.Text('About', style: pw.TextStyle(fontSize: 18))),
+                  pw.Container(width: 30, child: pw.Divider())
+                ]),
+
+            pw.Container(child: pw.Text(widget.about)),
+            pw.SizedBox(height: 30),
+
+            //Experience and Education
+            pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                children: [
+                  //Experience,Skills
+                  pw.Expanded(
+                      child: pw.Container(
+                          alignment: pw.Alignment.centerLeft,
+                          child: pw.Column(
+                              mainAxisAlignment: pw.MainAxisAlignment.start,
+                              crossAxisAlignment: pw.CrossAxisAlignment.start,
+                              children: [
+                                pw.Text('Experience',
+                                    style: pw.TextStyle(fontSize: 18)),
+                                pw.Container(width: 30, child: pw.Divider()),
+                                pw.Container(
+                                    child: pw.Text(
+                                        '$widget.jobTitle1, $widget.organization1',
+                                        style: pw.TextStyle(
+                                            fontSize: 16,
+                                            color: PdfColors.blue300))),
+                                //Date
+                                pw.Text(
+                                    'Date- $widget.startdate1 to $widget.enddate1'),
+                                //Achievement or Responsibility
+                                pw.Container(
+                                    height: 51,
+                                    child: pw.Bullet(
+                                        text: widget.experienceAbout1)),
+                                pw.SizedBox(height: 30),
+
+                                pw.Text(
+                                    '$widget.jobTitle2, $widget.organization2',
+                                    style: pw.TextStyle(
+                                        fontSize: 16,
+                                        color: PdfColors.blue300)),
+                                //Date
+                                pw.Text(
+                                    'Date- $widget.startdate2 to $widget.enddate2'),
+                                //Achievement or Responsibility
+                                pw.Container(
+                                    height: 51,
+                                    child: pw.Bullet(
+                                        text: widget.experienceAbout2)),
+                                pw.SizedBox(height: 30),
+
+                                //SKILLS
+                                pw.Text('Skills',
+                                    style: pw.TextStyle(fontSize: 18)),
+                                pw.Container(width: 30, child: pw.Divider()),
+                                pw.Text(widget.skills),
+                              ]))),
+                  //Education, Projects,
+                  pw.Expanded(
+                      child: pw.Container(
+                          alignment: pw.Alignment.centerLeft,
+                          child: pw.Column(
+                              mainAxisAlignment: pw.MainAxisAlignment.start,
+                              crossAxisAlignment: pw.CrossAxisAlignment.start,
+                              children: [
+                                pw.Text('Education',
+                                    style: pw.TextStyle(fontSize: 18)),
+                                pw.Container(width: 30, child: pw.Divider()),
+                                pw.Text(widget.instName1,
+                                    style: pw.TextStyle(
+                                        fontSize: 16,
+                                        color: PdfColors.blue300)),
+                                pw.Text(widget.course1),
+                                pw.SizedBox(height: 10),
+
+                                pw.Text('Date- $widget.edDate1'),
+                                pw.SizedBox(height: 15),
+                                pw.Text(widget.instName2,
+                                    style: pw.TextStyle(
+                                        fontSize: 16,
+                                        color: PdfColors.blue300)),
+                                pw.Text(widget.course2),
+                                pw.Text('Date- $widget.edDate2'),
+                                pw.SizedBox(height: 30),
+
+                                //Projects&Volunteer
+                                pw.Text('Projects & Volunteer',
+                                    style: pw.TextStyle(fontSize: 18)),
+                                pw.Container(width: 30, child: pw.Divider()),
+                                pw.SizedBox(height: 20),
+
+                                pw.Text('$widget.projTitle1',
+                                    style: pw.TextStyle(
+                                        fontSize: 16,
+                                        color: PdfColors.blue300)),
+
+                                //Achievement or Responsibility
+                                pw.Bullet(text: '$widget.projectDesc1'),
+                                pw.SizedBox(height: 20),
+                                pw.Text('$widget.projTitle2',
+                                    style: pw.TextStyle(
+                                        fontSize: 16,
+                                        color: PdfColors.blue300)),
+
+                                //Achievement or Responsibility
+                                pw.Bullet(text: '$widget.projectDesc2'),
+                              ])))
+                ]),
+          ]),
+        ),
+        index: 0);
+  }
+
+  var files;
   Future savePdf() async {
-    Directory dir = await getApplicationDocumentsDirectory();
+    Directory? dir = await getApplicationDocumentsDirectory();
     String documetPath = dir.path;
     File file = File("$documetPath/Resume.pdf");
 
-    file.writeAsBytesSync(await widget.pdf.save());
     setState(() {
       path = file.path;
+      isPathAvailable = true;
     });
   }
 
+  bool isPathAvailable = false;
 //comment
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton:
-            FloatingActionButton.extended(onPressed: null, label: Text('Save')),
-        body: Container(child: PdfViewer.openFile(path)));
+        appBar: AppBar(
+          title: Text('Resume'),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () async {
+              print(path);
+              Directory? dirs = await getExternalStorageDirectory();
+              File files = File("${dirs!.path}/Resume.pdf");
+              files.writeAsBytesSync(await widget.pdf.save());
+              print(files.path);
+            },
+            label: Text('Save')),
+        body: Container(
+            child: isPathAvailable
+                ? PdfViewer.openFile(
+                    path,
+                    params: PdfViewerParams(pageNumber: 1),
+                  )
+                : Text('Loading')));
   }
 }
